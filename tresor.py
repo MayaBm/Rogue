5a4c88af69292226dd37b7c75341bc966c0c2cbd
Sarah GRISLIN <sgrislin@e1r3p15.42.fr>
2018-11-13 15:09:44 +0100

    add monstre personnage tresor

---
+++ tresor.py
@@ -0,0 +1,16 @@
+class Tresor:
+    def __init__(self):
+        self.output = True
+        self.px = 5
+        self.py = 5
+
+
+
+def display_icon(self):
+    # display icon
+
+def remove_icon(self):
+    # remove icon ft
+
+
+
